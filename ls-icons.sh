37eabc89cafc663126df4d3e2a672009d10d8de7
ranebrown <rane.brown@gmail.com>
2019-01-04 12:33:08 -0700

    Make install an option of the main script

--- ls-icons.sh
+++ ls-icons.sh
@@ -288,3 +288,84 @@ else
     export LS_COLORS="rs=:ec=\e[0m:lc=:rc=:$LSC"
     alias ls='lsi --color=always --group-directories-first -Nh'
 fi
+
+main() {
+    help() {
+        echo "Useage: $0 [options]"
+        echo "-i download and build coreutils with ls patch, copy to /usr/local/bin"
+        echo
+        echo "Source this script to set LS_COLORS and"
+        echo "an alias to use the compiled ls with patch."
+        exit 0
+    }
+
+    # see this link for info on making subcommands
+    # https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
+    # leading ':' turns off default error handling
+    # options that require an argument have following ':' e.g. t:
+    while getopts ":hi" opt; do
+        case ${opt} in
+            h )
+                # print help message
+                help
+                ;;
+            i )
+                COREUTILS_VER=8.30
+
+                # Download coreutils and patch
+                wget https://ftp.gnu.org/gnu/coreutils/coreutils-${COREUTILS_VER}.tar.xz
+                if [[ $? == 5 ]]; then
+                    echo "Try using --ca-certificate= or --no-check-certificate with wget."
+                fi
+                tar -xf coreutils-${COREUTILS_VER}.tar.xz
+                rm coreutils-${COREUTILS_VER}.tar.xz
+                cd coreutils-${COREUTILS_VER} || exit 1
+                patch -p0 < ../ls.patch
+
+                # Compile and copy binaries
+                ./configure
+
+                if ! make; then
+                    if [[ "$(uname -s)" == "Darwin"* ]]; then
+                        echo "If make fails with a message like this:"
+                        echo "...built for archive which is not the architecture being linked (x86_64)"
+                        echo "Try \"brew unlink binutils\" and run script again."
+                    fi
+                fi
+
+                # copy the new executable
+                if [[ ! -e "/usr/local/bin/lsi" ]]; then
+                    mv src/ls /usr/local/bin/lsi
+                    echo "New executable 'lsi' installed to /usr/local/bin"
+                    echo "Make sure this is in your PATH."
+                    echo "Use something like \"alias ls='lsi'\" to use lsi in place of ls."
+                    cd ../ || exit 1
+                    rm -rf coreutils-${COREUTILS_VER}
+                else
+                    echo "ERROR: lsi already exists in /usr/local/bin" >&2
+                    exit 1
+                fi
+                ;;
+            
+            \? )
+                echo "Invalid option: $OPTARG" 1>&2
+                exit 1
+                ;;
+            : )
+                echo "Invalid option: $OPTARG requires an argument" 1>&2
+                exit 1
+                ;;
+        esac
+    done
+
+    # shift out processed parameters
+    shift $((OPTIND -1))
+    # error handling for non-option arguments
+    if [[ $# -ne 0 ]]; then
+        echo "Error: Got $# non-option arguments." >&2
+        exit 1
+    fi
+}
+
+# script can be sourced or run directly
+[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
